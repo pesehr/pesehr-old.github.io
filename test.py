15c586cc53da9ec1be7bba72b35748714864f74a
pesehr <sepehr.sabour@gmail.com>
2019-09-24 08:51:46 -0600

    Worksheet 3

---
+++ test.py
@@ -0,0 +1,13 @@
+age = int(input("Enter your age!"))
+state = input("Enter State")
+
+if state == 'Alberta':
+    if age > 17:
+        print("You can buy alcohol")
+    if age <= 17:
+        print("You cannot buy alcohol")
+if state == 'Quebec':
+    if age > 18 and :
+        print("You can buy alcohol")
+    if age <= 18:
+        print("You cannot buy alcohol")
