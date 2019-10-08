24845a54d53f3a17314b1ed9625af805db9b1b45
pesehr <sepehr.sabour@gmail.com>
2019-10-08 08:46:40 -0600

    Add exercise 7

--- files/exercise07.py
+++ files/exercise07.py
@@ -5,22 +5,30 @@ WIDTH = 800
 HEIGHT = 600
 RATIO = 5
 
-
+# setting up the screen
 screen = turtle.getscreen()
 screen.setup(WIDTH, HEIGHT, 0,0)
 screen.setworldcoordinates(0,0, WIDTH, HEIGHT)
 turtle.speed(90)
 
+
+# Draw Y axis
 turtle.penup()
 turtle.goto(WIDTH / 2,0)
 turtle.pendown()
 turtle.goto(WIDTH / 2,HEIGHT)
 
+# Draw X axis
 turtle.penup()
 turtle.goto(0,HEIGHT/2)
 turtle.pendown()
 turtle.goto(WIDTH,HEIGHT/2)
 
+# X axis line ticks
+#TODO
+
+# Y axis line ticks
+#TODO
 
 
 turtle.exitonclick()
